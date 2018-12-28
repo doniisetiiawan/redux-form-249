556615615558a619d4f6b5898165ada55ce1e115
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-28 19:44:15 +0700

    Add reactpopup to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Popup from "react-popup";
 import Header from "../shared/components/layout/Header";
 import Content from "../shared/components/layout/Content";
 import Footer from "../shared/components/layout/Footer";
 
 import Person from "./Person/Person";
 import "./App.css";
+import "./Popup.css";
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
           <Person />
         </Content>
         <Footer />
+        <Popup />
       </div>
     );
   }
