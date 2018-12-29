cf2b22ed1bd7ba01fe57d37b5d6ad730e1f209b6
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-29 19:10:37 +0700

    Add other component

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 import Error404 from './components/Error/404';
 
 const AppRoutes = () => (
   <App>
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} exact />
+      <Route path="/contact" component={Contact} exact />
       <Route component={Error404} />
     </Switch>
   </App>
