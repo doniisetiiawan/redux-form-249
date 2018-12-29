09d2942799f520d6ac16fe7f4355cd65977afab3
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-29 19:05:35 +0700

    Add error component to route

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 
 import App from './components/App';
 import Home from './components/Home';
+import Error404 from './components/Error/404';
 
 const AppRoutes = () => (
   <App>
     <Route path="/" component={Home} />
+    <Route component={Error404} />
   </App>
 );
 
