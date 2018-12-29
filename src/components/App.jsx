41975da161d1154c85183b16c184368cad52a8ba
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-29 18:36:22 +0700

    Refactor app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Popup from 'react-popup';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
-
-import Person from './Person/Person';
 import './App.css';
 import './Popup.css';
 
-const App = () => (
+const App = props => (
   <div className="App">
     <Header title="Welcome to Codejobs" />
+
     <Content>
-      <Person />
+      {props.children}
     </Content>
+
     <Footer />
     <Popup />
   </div>
 );
+
+App.propTypes = {
+  children: PropTypes.element,
+};
 
 export default App;
