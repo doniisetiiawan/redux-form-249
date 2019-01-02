ed5ae4b12cd7c059e7efa2e1efd35edc274a4820
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 11:10:00 +0700

    Refactor home to connect to redux

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Home = () => (
-  <div className="Home">
-    <h1>Home</h1>
-  </div>
-);
-export default Home;
+const Home = (props) => {
+  const { isMobile } = props;
+
+  return (
+    <div className="Home">
+      <h1>Home</h1>
+      <p>
+        You are using:
+        <strong> {isMobile ? 'mobile' : 'desktop'}</strong>
+      </p>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  isMobile: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+  isMobile: state.device.isMobile,
+});
+
+export default connect(mapStateToProps)(Home);
