1c526a35fa47443d1c91c2ed788fe5305841226a
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 11:09:17 +0700

    Refactor index create redux store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import configureStore from './shared/redux/configureStore';
+
 import AppRoutes from './routes';
 
-render(
-  <Router>
-    <AppRoutes />
-  </Router>,
-  document.getElementById('root'),
-);
+const store = configureStore(window.initialState);
+
+const rootElement = document.getElementById('root');
+
+const renderApp = (Component) => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Component />
+      </Router>
+    </Provider>,
+    rootElement,
+  );
+};
+
+renderApp(AppRoutes);
