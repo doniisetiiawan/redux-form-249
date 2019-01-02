d2886184186fbb0fa88dd0dbc4d1b886dc4fa271
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 12:18:45 +0700

    Add coins reducer to root reducer

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import device from './deviceReducer';
+import coins from '../../reducers/coinsReducer';
 
 const rootReducer = combineReducers({
   device,
+  coins,
 });
 
 export default rootReducer;
