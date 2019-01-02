eb70513b4fe4f7fc72678d20b0fc9c4cc62506d6
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 09:53:52 +0700

    Add detail notes

--- src/components/Notes/index.jsx
+++ src/components/Notes/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './Notes.css';
 
 class index extends Component {
@@ -23,13 +25,41 @@ class index extends Component {
     };
   }
 
+  renderNotes = notes => (
+    <ul>
+      {notes.map((note, key) => (
+        <li key={key}>
+          <Link to={`/notes/${note.id}`}>{note.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+
   render() {
+    console.log(this.props);
+
+    const { match: { params: { noteId } } } = this.props;
+    const { notes } = this.state;
+
+    let selectedNote = false;
+
+    if (noteId > 0) {
+      selectedNote = notes.filter(
+        note => note.id === Number(noteId),
+      );
+    }
+
     return (
       <div className="Notes">
         <h1>Notes</h1>
+        {this.renderNotes(selectedNote || notes)}
       </div>
     );
   }
 }
+
+index.propTypes = {
+  match: PropTypes.shape(PropTypes.object),
+};
 
 export default index;
