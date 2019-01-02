1e42c7ecf505a8f40650baf9e9fca1e48b33bdeb
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 12:45:50 +0700

    Add component to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Home from './components/Home';
 import About from './components/About';
+import Coins from './components/Coins';
 import Contact from './components/Contact';
 import Notes from './components/Notes';
 import Error404 from './components/Error/404';
@@ -13,6 +14,7 @@ const AppRoutes = () => (
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
+      <Route path="/coins" component={Coins} exact />
       <Route path="/contact" component={Contact} exact />
       <Route path="/notes" component={Notes} exact />
       <Route path="/notes/:noteId" component={Notes} exact />
