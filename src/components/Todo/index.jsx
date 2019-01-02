a210d560006fa9c71cd72d0a5797a0d314932caa
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 17:27:39 +0700

    Refactor todo component

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import uuidv4 from 'uuid/v4';
+import List from './List';
 import './Todo.css';
 
-const index = () => (
-  <div className="Todo">
-    <h1>New Task:</h1>
-  </div>
-);
+class index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: '',
+      items: [],
+    };
+  }
+
+  handleOnChange = (e) => {
+    const { target: { value } } = e;
+
+    this.setState({
+      task: value,
+    });
+  };
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      task: '',
+      items: [
+        ...this.state.items,
+        {
+          id: uuidv4(),
+          task: this.state.task,
+          complete: false,
+        },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="Todo">
+        <h1>New Task:</h1>
+
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            value={this.state.task}
+            onChange={this.handleOnChange}
+          />
+        </form>
+
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+}
 
 export default index;
